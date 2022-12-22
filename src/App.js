e8ad0972c0bcfaddade016019f04837e56472f66
peternabiswa <peter.nabiswa@gmail.com>
2022-12-22 06:15:23 +0300

    Added props

--- src/App.js
+++ src/App.js
@@ -1,15 +1,33 @@
 import styles from "./app.module.css";
 import { useState, useEffect } from "react";
 import GetUserInfo from "./GetUserInfo.js";
+import { userName } from "./GetUserInfo.js";
 
 function App() {
+  const [userData, setUserData] = useState({});
+  const [userName, setUserName] = useState("");
+
+  const getName = (name) => {
+    setUserName(name);
+  };
+  async function getData(userName) {
+    const jsonData = await fetch(`https://api.github.com/users${userName}`);
+    const userInfo = await jsonData.json();
+    setUserData(userInfo);
+  }
+
+  useEffect((userName) => {
+    getData(userName);
+  }, []);
+
   return (
     <div className="App">
       <div className={styles.container}>
         <main>
           <h1 className={styles.mainh1}>Find Users</h1>
           <div className={styles.topbar}></div>
-          <GetUserInfo />
+          <GetUserInfo uname={getName} />
+          <Users user={userData} />;
         </main>
       </div>
     </div>
