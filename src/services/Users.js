df093e9df202a555785a6f2443d0595e847c8f62
peternabiswa <peter.nabiswa@gmail.com>
2023-01-11 19:32:32 -0800

    Dislaying more user attributes and Git profile link

--- src/services/Users.js
+++ src/services/Users.js
@@ -7,6 +7,11 @@ const Users = (props) => {
       <div>Name: {userData.name}</div>
       <div>userName: {userData.login}</div>
       <div>ID: {userData.id}</div>
+      <div>Followers: {userData.id}</div>
+      <div>Following: {userData.id}</div>
+      <div>
+        <a href={userData.html_url}>Repository</a>
+      </div>
       <div>
         <img src={userData.avatar_url} alt="Profile picture"></img>
       </div>
